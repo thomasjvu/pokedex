6e3c539892fbd441374270520dc5f6524964ab16
Thomas Vu <thomasjvu@gmail.com>
2023-05-02 02:45:55 -0400

    declare module for svg files

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,8 +1,10 @@
+import Pokeball from '../assets/img/pokeball.svg'
+
 export default function Loader() {
     
     return (
         <div>
-            <img id="loader" src='../assets/img/pokeball.svg' width="50px" />
+            <img id="loader" src={Pokeball} width="50px" />
         </div>
     )
 }
